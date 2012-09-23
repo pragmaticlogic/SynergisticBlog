018858349c2f415844606ce33fcc739d89a78822
Kevin Le <pragmaticlogic@gmail.com>
2012-09-22 21:26:11 -0500

    Change connection string at runtime

--- SynergisticBlog/Global.asax.cs
+++ SynergisticBlog/Global.asax.cs
@@ -8,6 +8,8 @@ using System.Web.Mvc;
 using System.Web.Routing;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Xml.Linq;
+using System.Data.EntityClient;
 
 namespace SynergisticBlog
 {
@@ -50,10 +52,52 @@ namespace SynergisticBlog
                 Password = uri.UserInfo.Split(':').Last(),
             }.ConnectionString;
 
-            Database.DefaultConnectionFactory = new SqlConnectionFactory(ConfigurationManager.AppSettings["SQLSERVER_CONNECTION_STRING"]);
+            //Database.DefaultConnectionFactory = new SqlConnectionFactory(ConfigurationManager.AppSettings["SQLSERVER_CONNECTION_STRING"]);
+            ChangeEFConnectionString("DefaultConnection", connectionString);
 
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
+        }
+
+        private bool ChangeEFConnectionString(string connStringName, string newValue)
+        {
+            try
+            {
+                //CreateXDocument and load configuration file
+                XDocument doc = XDocument.Load(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+
+                //Find all connection strings
+                var query1 = from p in doc.Descendants("connectionStrings").Descendants()
+                             select p;
+
+                //Go through each connection string elements find atribute specified by argument and replace its value with newVAlue
+                foreach (var child in query1)
+                {
+                    foreach (var atr in child.Attributes())
+                    {
+                        if (atr.Name.LocalName == "name" && atr.Value == connStringName)
+                            if (atr.NextAttribute != null && atr.NextAttribute.Name == "connectionString")
+                            {
+                                // Create the EF connection string from existing
+                                EntityConnectionStringBuilder entityBuilder =
+                                new EntityConnectionStringBuilder(atr.NextAttribute.Value);
+                                //
+                                entityBuilder.ProviderConnectionString = newValue;
+                                //back the modified connection string to the configuration file
+                                atr.NextAttribute.Value = entityBuilder.ToString();
+                            }
+                    }
+                }
+
+                doc.Save(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
         }
     }
 }
