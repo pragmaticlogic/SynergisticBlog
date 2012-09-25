257493f8bace42eedbc07974e5cd9951ea6c9754
Kevin Le <pragmaticlogic@gmail.com>
2012-09-24 23:11:33 -0500

    SQLSERVER_CONNECTION_STRING

--- SynergisticBlog/Global.asax.cs
+++ SynergisticBlog/Global.asax.cs
@@ -6,10 +6,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Configuration;
-using System.Data.SqlClient;
-using System.Xml.Linq;
-using System.Data.EntityClient;
 
 namespace SynergisticBlog
 {
@@ -35,65 +31,15 @@ namespace SynergisticBlog
 
         }
 
-        protected virtual void Application_PreRequestHandlerExecute(Object sender, EventArgs e)
-        {
-            //ChangeConnectionString("DefaultConnection", ConfigurationManager.AppSettings["SQLSERVER_CONNECTION_STRING"]);
-        }
-
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
 
             // Use LocalDB for Entity Framework by default
-            //Database.DefaultConnectionFactory = new SqlConnectionFactory(@"Data Source=(localdb)\v11.0; Integrated Security=True; MultipleActiveResultSets=True");
-            
-            var uriString = ConfigurationManager.AppSettings["SQLSERVER_URI"];
-            var uri = new Uri(uriString);
-            var connectionString = new SqlConnectionStringBuilder
-            {
-                DataSource = uri.Host,
-                InitialCatalog = uri.AbsolutePath.Trim('/'),
-                UserID = uri.UserInfo.Split(':').First(),
-                Password = uri.UserInfo.Split(':').Last(),
-            }.ConnectionString;
-
-            //Database.DefaultConnectionFactory = new SqlConnectionFactory(ConfigurationManager.AppSettings["SQLSERVER_CONNECTION_STRING"]);            
+            Database.DefaultConnectionFactory = new SqlConnectionFactory(@"Data Source=(localdb)\v11.0; Integrated Security=True; MultipleActiveResultSets=True");
 
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
-        }
-
-        private bool ChangeConnectionString(string connStringName, string newValue)
-        {
-            try
-            {
-                //CreateXDocument and load configuration file
-                XDocument doc = XDocument.Load(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
-
-                //Find all connection strings
-                var query1 = from p in doc.Descendants("connectionStrings").Descendants()
-                             select p;
-
-                //Go throught each connection string elements find atribute specified by argument and replace its value with newVAlue
-                foreach (var child in query1)
-                {
-                    foreach (var atr in child.Attributes())
-                    {
-                        if (atr.Name.LocalName == "name" && atr.Value == connStringName)
-                            if (atr.NextAttribute != null && atr.NextAttribute.Name == "connectionString")
-                                atr.NextAttribute.Value = newValue;
-                    }
-                }
-
-                doc.Save(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
-
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return false;
-            }
         }
     }
 }
---
+++ SynergisticBlog/Security/CustomMembershipProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Profile;
+using System.Web.Security;
+using System.Configuration;
+
+namespace SynergisticBlog.Security
+{
+    public class CustomMembershipProvider : SqlMembershipProvider
+    {
+        public override void Initialize(string name, System.Collections.Specialized.NameValueCollection config)
+        {
+            base.Initialize(name, config);// Update the private connection string field in the base class.
+            string connectionString = ConfigurationManager.AppSettings["SQLSERVER_CONNECTION_STRING"];
+            // Set private property of Membership provider.
+            var connectionStringField = GetType().BaseType.GetField("_sqlConnectionString", BindingFlags.Instance | BindingFlags.NonPublic);
+            connectionStringField.SetValue(this, connectionString);
+        }
+    }
+}
