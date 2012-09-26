93447adf5c95d2fc1b6afd0f386070b3876b0e45
Kevin Le <pragmaticlogic@gmail.com>
2012-09-26 07:54:19 -0500

    incorporate changes

---
+++ SynergisticBlog/Controllers/AdminController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SynergisticBlog.Models;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Driver.Builders;
+
+namespace SynergisticBlog.Controllers
+{
+    public class AdminController : BaseController
+    {
+        [Authorize]
+        public ActionResult Index(string page, bool update, string id)
+        {
+            Post post = null;
+
+            if (update)
+            {
+                post = _collection.FindOneById(ObjectId.Parse(id));
+            }
+            else
+            {
+                post = new Post()
+                {
+                    Page = page,
+                    TimeCreated = DateTime.MinValue,
+                };
+            }
+            return View(post);
+        }
+
+        [Authorize]
+        [ValidateInput(false)]
+        [HttpPost]
+        public ActionResult Create(Post post)
+        {
+            var query = Query.EQ("UUID", post.UUID);
+            var update = Update.Set("Title", post.Title)
+                .Set("Content", post.Content);
+
+            if (post.TimeCreated == DateTime.MinValue)
+            {
+                post.TimeCreated = DateTime.Now;
+                post.UUID = System.Guid.NewGuid().ToString();
+                _collection.Insert(post);
+            }
+            else
+            {
+                _collection.Update(query, update);
+            }
+            
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ SynergisticBlog/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using SynergisticBlog.Models;
+
+namespace SynergisticBlog.Controllers
+{
+    public class PostController : BaseController
+    {
+        public ActionResult Index(string id, string page)
+        {
+            var post = _collection.FindOneById(ObjectId.Parse(id));
+            return View(post);
+        }
+    }
+}
